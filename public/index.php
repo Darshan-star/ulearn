c24ead32a44531911a03509e5c1bd1126bd9e05b
Darshan <75985193+Darshan-star@users.noreply.github.com>
2023-01-30 16:08:49 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
- * @package  Laravel
+ * @package  Laravel 
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
@@ -19,7 +19,7 @@ define('LARAVEL_START', microtime(true));
 | into the script here so that we don't have to worry about manual
 | loading any of our classes later on. It feels great to relax.
 |
-*/
+*/ 
 
 require __DIR__.'/../vendor/autoload.php';
 
@@ -29,14 +29,14 @@ require __DIR__.'/../vendor/autoload.php';
 |--------------------------------------------------------------------------
 |
 | We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
+| This bootstraps the framework and gets it ready for use, then it 
 | will load up this application so that we can run it and send
 | the responses back to the browser and delight our users.
 |
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
+ 
 /*
 |--------------------------------------------------------------------------
 | Run The Application
@@ -52,9 +52,10 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $request = Illuminate\Http\Request::capture() 
 );
-
+ 
 $response->send();
 
 $kernel->terminate($request, $response);
+ 
